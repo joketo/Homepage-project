3e4f0a664911aa3802afb450e12d93911f77ace4
Johanna <johanna.keto@helsinki.fi>
2017-03-15 21:00:44 +0200

    Added Katas coat and improvements to the paintings page

--- prettyPhoto_3.1.6/js/myconfig.js
+++ prettyPhoto_3.1.6/js/myconfig.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".openWeddingImage").click(function(){
+    $(".openElliWeddingImages").click(function(){
         api_images = ['CLOTHES/ellinmekkofull.JPG','CLOTHES/ellinmekkofullback.JPG','CLOTHES/ellinmekkofront.JPG','CLOTHES/ellinmekkoback.JPG','CLOTHES/ellinmekkofront2.JPG'];
         api_titles = ['Wedding dress','','','',''];
         api_descriptions = ['The dress is made with my friends measurements. It\'s supposed to be really light and delicate like her.','The hem is slightly fuller from behind.','Part of the lace is appliqued by hand. The neckline is hand-stitched.','The lace in the back is mostly hand appliqued. The buttons are made from the same fabric as the hem.','The sleeves are one of my favourite part of the dress.']
@@ -12,9 +12,21 @@ $(document).ready(function(){
         $.prettyPhoto.open(images,titles,descriptions);
     });
     $(".openWeddingImages").click(function(){
-        wimages = ['CLOTHES/wedding.png','CLOTHES/weddinghem.JPG','CLOTHES/wedding2.JPG'];
+        wimages = ['CLOTHES/wedding.png','CLOTHES/weddinghem.JPG','CLOTHES/wedding2.png','CLOTHES/wedding3.png'];
         wtitles = ['The wedding dress','',''];
-        wdescriptions = ['The green lace makes this wedding dress less traditional.','The hem has pearls and real flowers.','Organza cape made of silk is part of the dress']
+        wdescriptions = ['The green lace makes this wedding dress less traditional.','The hem has pearls and real flowers.','Organza cape made of silk is part of the dress','The hem has many layers but is still light to wear.']
+        $.prettyPhoto.open(wimages,wtitles,wdescriptions);
+    });
+    $(".openLightJacketImages").click(function(){
+        wimages = ['CLOTHES/takki1.JPG','CLOTHES/takki2.JPG','CLOTHES/takki_kaulus.JPG','CLOTHES/takki_hiha.JPG'];
+        wtitles = ['Light jacket','',''];
+        wdescriptions = ['Cotton jacket for light workout such as biking in the city.','','Detail of the neckline.','The inside of the sleeves are gray fabric.']
+        $.prettyPhoto.open(wimages,wtitles,wdescriptions);
+    });
+    $(".openKataCoatImages").click(function(){
+        wimages = ['CLOTHES/katatakki_front.JPG','CLOTHES/katatakki_sivusta.JPG','CLOTHES/katatakki_takaa.JPG','CLOTHES/katatakki_kaulus.JPG','CLOTHES/katatakki_nappi.JPG'];
+        wtitles = ['Wool coat','',''];
+        wdescriptions = ['My friend designed this wool coat for herself. I made the pattern and technical choices based on her drawing.','The model of the coat is really unique. It took many fittings to get it just right.','The back has the same black pattern as the front.','The neckline can be worn in two different ways.','The buttons are gorgeous.']
         $.prettyPhoto.open(wimages,wtitles,wdescriptions);
     });
     
