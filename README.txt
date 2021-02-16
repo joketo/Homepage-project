6ab03f083cb581969cf6f8c32273d569a611b111
Johanna <joketona@gmail.com>
2021-02-16 22:16:29 +0200

    demo for new gallery

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/teeganlincoln/pen/MaNxBv.
+
+ A gallery that reveals three panels as the user scrolls.
---
+++ js/index.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+   var activeImage = document.querySelector(".animate .active");
+   var productImages = document.querySelectorAll(".tiles img");
+    
+   var activeImages = document.querySelectorAll(".active");
+   var navItem = document.querySelector('a.toggle-nav');
+
+   function changeImage(e) {
+       var activeImage = document.querySelector(".animate .active");
+       activeImage.src = e.target.src;
+   }
+   function changeImageTouch(e) {
+       console.log("hoi", e);
+       var activeImage = document.querySelector(".animate .active");
+       activeImage.src = e.target.src;
+   }
+
+   function toggleNavigation(){
+      this.nextElementSibling.classList.toggle('active');
+   }
+
+   productImages.forEach(image => image.addEventListener("click", changeImage));
+
+   activeImages.forEach(image => image.addEventListener('touchstart', changeImageTouch));
+                               
+   var $sm = 480;
+   var $md = 768;
+
+   function resizeThis() {
+      $imgH = $('.middle img').width();
+      if ($(window).width() >= $sm) {
+         $('.left,.right,.section').css('height', $imgH);
+      } else {
+         $('.left,.right,.section').css('height', 'auto');
+      }
+   }
+
+   resizeThis();
+
+   $(window).resize(function(){
+      resizeThis();
+   });
+
+   $(window).scroll(function() {
+      $('.section').each(function(){
+         var $elementPos = $(this).offset().top;
+         var $scrollPos = $(window).scrollTop();
+
+         var $sectionH = $(this).height();
+         var $h = $(window).height();
+         var $sectionVert = (($h/2)-($sectionH/4));
+
+
+         if (($elementPos - $sectionVert) <= $scrollPos && ($elementPos - $sectionVert) + $sectionH > $scrollPos) {
+            $(this).addClass('animate');
+         } else {
+            $(this).removeClass('animate');
+         }
+      });
+   });
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
